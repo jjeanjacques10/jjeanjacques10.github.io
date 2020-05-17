dd90d1de2121f069311b1d323bf7e200db2f1aac
jjeanjacques10 <jjean.jacques10@gmail.com>
2020-05-17 06:07:55 -0300

    Limpando a classe articles

--- js/articles.js
+++ js/articles.js
@@ -1,4 +1,3 @@
-var tableelement = $(".imageGallery1");
 //ajax call
 $.ajax({
     url: "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@jjean.jacques10",
@@ -6,7 +5,6 @@ $.ajax({
     dataType: "json"
 }).done(function (data, textStatus, jqXHR) {
     data['items'].forEach(element => {
-        description = clean_description(element["description"]);
         guid = element["guid"];
         thumbnail = element["thumbnail"];
         title = element["title"];
@@ -16,44 +14,30 @@ $.ajax({
         year = date.getFullYear();
 
         var item =
-            '<div class="col-lg-3 col-md-3 col-sm-6 ' + year + '">' +
-            '<div class="h_gallery_item">' +
-            '<div class="g_img_item">' +
-            '<img class="img-fluid" src="' + thumbnail + '" alt="">' +
-            '<a class="light" href="' + thumbnail + '"><img src="img/talks/icon.png" alt=""></a>' +
-            '</div>' +
-            '<div class="g_item_text">' +
-            '<a href="' + guid + '" target="_blank"' +
-            'title="Abre em nova aba" rel="noopener noreferrer">' +
-            '<h4><span lang="en">' + title + '</span></h4>' +
-            '</a>' +
-            '<p class="event_name"><span lang="en">Medium</span></p>' +
-            '<div class="links">' +
-            '<a href="' + guid + '" target="_blank" title="Abre em nova aba" rel="noopener noreferrer"><span class="button" id="pt">Artigo</span></a>' +
-            '<a href="' + guid + '" target="_blank" title="Abre em nova aba" rel="noopener noreferrer"><span class="button" id="en">Article</span></a>' +
-            '</div>' +
-            '<div class="metadata">' +
-            '<p>São Paulo (SP) - Brasil</p>' +
-            '<p id="pt">' + day + ', ' + month + ' - ' + year + '</p>' +
-            '<p id="en">' + month + ', ' + day + ' - ' + year + '</p>' +
-            '</div>' +
-            '</div>' +
-            '</div>' +
-            '</div>';
-        tableelement.append(item);
+            "<div class=\"col-lg-3 col-md-3 col-sm-6 " + year + "\">" +
+            "<div class=\"h_gallery_item\">" +
+            "<div class=\"g_img_item\">" +
+            "<img class=\"img-fluid\" src=\"" + thumbnail + "\" alt=\"\">" +
+            "<a class=\"light\" href=\"" + thumbnail + "\"><img src=\"img/talks/icon.png\" alt=\"\"></a>" +
+            "</div>" +
+            "<div class=\"g_item_text\">" +
+            "<a href=\"" + guid + "\" target=\"_blank\"" +
+            "title=\"Abre em nova aba\" rel=\"noopener noreferrer\">" +
+            "<h4><span lang=\"en\">" + title + "</span></h4>" +
+            "</a>" +
+            "<p class=\"event_name\"><span lang=\"en\">Medium</span></p>" +
+            "<div class=\"links\">" +
+            "<a href=\"" + guid + "\" target=\"_blank\" title=\"Abre em nova aba\" rel=\"noopener noreferrer\"><span class=\"button\" id=\"pt\">Artigo</span></a>" +
+            "<a href=\"" + guid + "\" target=\"_blank\" title=\"Abre em nova aba\" rel=\"noopener noreferrer\"><span class=\"button\" id=\"en\">Article</span></a>" +
+            "</div>" +
+            "<div class=\"metadata\">" +
+            "<p>São Paulo (SP) - Brasil</p>" +
+            "<p id=\"pt\">" + day + ", " + month + " - " + year + "</p>" +
+            "<p id=\"en\">" + month + ", " + day + " - " + year + "</p>" +
+            "</div>" +
+            "</div>" +
+            "</div>" +
+            "</div";
+        $(".imageGallery1").append(item);
     });
-
 });
-
-function clean_description(description) {
-    description = description.substring(0, 90);
-    description = description.replace("<h3>", "<p>");
-    cut = description.search('<\/h3>\s\w+');
-    description = description.replace(cut, "  ");
-    description = description.replace("<h4>", "<p>");
-    cut = description.search('<\/h4>\s\w+');
-    description = description.replace(cut, "  ");
-    description += '...</p>';
-    console.log(description);
-    return description;
-}
